ab8abab32320166eecf730bae3a56f31a449d429
Chris M <xplowersu@gmail.com>
2018-05-13 21:11:06 -0400

    added a cursor

--- create_training_data.py
+++ create_training_data.py
@@ -1,7 +1,14 @@
 import sqlite3
-import pandas
+import pandas as pd
 
 timeframes = ['2015-02']
 
 for timeframe in timeframes:
+    connection = sqlite3.connect('{}.db'.format(timeframe))
+    c = connection.cursor()
+    limit = 5000
+    last_unix = 0
+    cur_length = limit
+    counter = 0
+    test_done = False
     
