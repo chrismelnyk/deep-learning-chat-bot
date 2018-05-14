8a8e9ee1c3967bff0efa086f81b6ba366c01d0eb
Chris M <xplowersu@gmail.com>
2018-05-13 21:39:22 -0400

    created new loop for parent and comment

--- create_training_data.py
+++ create_training_data.py
@@ -11,4 +11,16 @@ for timeframe in timeframes:
     cur_length = limit
     counter = 0
     test_done = False
-    
+    while cur_length == limit:
+        df = pd.read_sql("SELECT * FROM parent_reply WHERE unix > {} AND parent NOT NULL AND score > 0 ORDER BY unix ASC LIMIT {}".format(last_unix, limit), connection)
+        last_unix = df.tail(1)['unix'].values[0]
+        cur_length = len(df)
+        if not test_done:
+            with open("test.from",'a', encoding='utf8') as f:
+                for content in df['parent'].values:
+                    f.write(content+'\n')
+            with open("test.to",'a', encoding='utf8') as f:
+                for content in df['comment'].values:
+                    f.write(content+'\n')
+
+            test_done = True
